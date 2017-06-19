451123ab10514d6fb0e957539440ee267d885b59
jojoldu <jojoldu@gmail.com>
2017-06-19 11:57:23 +0900

    추가한 정적 파일들 의존성 목록에 추가

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,8 +1,6 @@
 
 import Woowahan from 'woowahan';
 
-// import './css/service/common.css';
-
 //vender
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
@@ -10,3 +8,14 @@ import 'magnific-popup/dist/jquery.magnific-popup.min';
 import 'simple-line-icons/css/simple-line-icons.css';
 import 'owl.carousel/dist/assets/owl.carousel.min.css';
 import 'owl.carousel/dist/assets/owl.theme.default.min.css';
+import './css/vender/animate.min.css';
+import './css/vender/layers.css';
+import './css/vender/navigation.css';
+
+//service
+import './css/service/demos/demo-shop-5.css';
+import './css/service/skins/skin-shop-5.css';
+import './css/service/theme.css';
+import './css/service/theme-blog.css';
+import './css/service/theme-elements.css';
+import './css/service/theme-shop.css';
