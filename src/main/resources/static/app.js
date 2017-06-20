197266743a07cbfc380931aa70fe2b86b0dbebca
jojoldu <jojoldu@gmail.com>
2017-06-21 08:56:54 +0900

    테마 깨진 것 복구

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -11,11 +11,13 @@ import 'owl.carousel/dist/assets/owl.theme.default.min.css';
 import './css/vender/animate.min.css';
 import './css/vender/layers.css';
 import './css/vender/navigation.css';
+import './css/vender/rs-plugin/css/settings.css';
+import './css/vender/style-switcher/style-switcher.css';
 
 //service
-import './css/service/demos/demo-shop-5.css';
-import './css/service/skins/skin-shop-5.css';
 import './css/service/theme.css';
 import './css/service/theme-blog.css';
 import './css/service/theme-elements.css';
 import './css/service/theme-shop.css';
+import './css/service/demos/demo-shop-5.css';
+import './css/service/skins/skin-shop-5.css';
---
+++ src/main/resources/static/css/vender/rs-plugin/README.md
@@ -0,0 +1,7 @@
+# rs-plugin
+
+```zsh
+bower install rs-plugin --save
+```
+
+This is only a bower repository, if you need source code, please visit [http://revolution.themepunch.com/](http://revolution.themepunch.com/) to buy it.
